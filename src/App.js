49aae2c34036673772b8e0dd8e3c747717e8ed9b
RythBiss <dkschaarschmidt@gmail.com>
2023-01-26 15:28:31 -0500

    Basic Grinder Data
    Added basic grinder/tooling data to test filter function.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { createBrowserRouter, createRoutesFromElements, Link, Outlet, Route, RouterProvider } from "react-router-dom";
 import Home from './Components/Home';
-import Products from './Components/Products'
+import About from './Components/About';
+import Products from './Components/Products';
 import ApplicationSearch from './Components/ApplicationSearch';
-import CustomerSupport from './Components/CustomerSupport'
-import BuyEDCO from './Components/BuyEDCO'
-import Training from './Components/Training'
-import Login from './Components/Login'
-import Register from './Components/Register'
+import CustomerSupport from './Components/CustomerSupport';
+import BuyEDCO from './Components/BuyEDCO';
+import Training from './Components/Training';
+import Login from './Components/Login';
+import Register from './Components/Register';
 
 function App() {
 
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Root />}>
       <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
       <Route path='products' element={<Products />} />
       <Route path='customersupport' element={<CustomerSupport />} />
       <Route path='buyedco' element={<BuyEDCO />} />
@@ -35,19 +37,23 @@ const router = createBrowserRouter(
 
 const Root =() => {
   return <>
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/products'>Products</Link>
-      <Link to='/customersupport'>Cutomer Support</Link>
-      <Link to='/buyedco'>Buy EDCO</Link>
-      <Link to='/training'>Training</Link>
-      <Link to='/applicationfinder'>Search by Application</Link>
-      <Link to='/login'>Login</Link>
-      <Link to='/register'>Register</Link>
-    </nav>
+    <header>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/products'>Products</Link>
+        <Link to='/customersupport'>Cutomer Support</Link>
+        <Link to='/buyedco'>Buy EDCO</Link>
+        <Link to='/training'>Training</Link>
+        <Link to='/applicationfinder'>Search by Application</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+      </nav>
+    </header>
     <main>
       <Outlet />
     </main>
+    <footer>footer</footer>
   </>
 }
 
--- src/Components/ApplicationSearch.js
+++ src/Components/ApplicationSearch.js
@@ -1,7 +1,91 @@
 import React from 'react'
 
 export default function ApplicationSearch() {
+
+  const tags = [
+    'generalSmoothing',
+    'urethanes',
+    'epoxy',
+    'highSpots',
+    'rainMarks',
+    'mastic',
+    'thinset',
+    'paint',
+    'nonSlip',
+    'industrialCoatings',
+    'edges',
+    'buildUp',
+    'coatings',
+    'resin',
+    'surfacePrep',
+    'glue'
+  ]
+
+  const machineObjects ={
+    singleDisc: {
+      name: 'SEC',
+      power: ['gas'],
+      range: 'small',
+      tags: ['generalSmoothing', 'urethanes', 'epoxy', 'highSpots', 'mastic', 'thinset', 'paint', 'nonSlip', 'glue']
+    },
+    dualDisc: {
+      name: '2EC-NG',
+      power: ['gas', 'electric'],
+      range: 'large',
+      tags: ['generalSmoothing', 'urethanes', 'epoxy', 'highSpots', 'mastic', 'thinset', 'paint', 'nonSlip', 'glue']
+    },
+    heavyDualDisc: {
+      name: '2D-HD',
+      power: ['gas', 'electric'],
+      range: 'large',
+      tags: ['industrialCoverings', 'highSpots']
+    },
+    edgeTurboGrinder: {
+      name: 'TMC7',
+      power: ['gas', 'electric'],
+      range: 'small',
+      tags: ['highSpots', 'edges', 'buildUp', 'coatings', 'resin', 'surfacePrep']
+    },
+    lightTurboGrinder: {
+      name: 'TL9',
+      power: ['electric'],
+      range: 'small',
+      tags: ['highSpots', 'edges', 'buildUp', 'coatings', 'resin', 'surfacePrep']
+    },
+    turboGrinder: {
+      name: 'TG-10',
+      power: ['gas', 'electric', 'propane'],
+      range: 'small',
+      tags: ['highSpots', 'buildUp', 'coatings', 'resin', 'surfacePrep']
+    },
+  }
+
+  const magnaTrapTools = {
+    blades: {
+      finish: 'smooth',
+      tags: ['glue', 'paint']
+    },
+    doubleDots: {
+      finish: 'smooth',
+      tags: ['epoxy', 'mastic', 'thinset', 'coatings', 'generalSmoothing', 'urethanes', 'highSpots', 'rainMarks', 'buildUp', 'resin', 'surfacePrep']
+    },
+    segs: {
+      finish: 'textured',
+      tags: ['epoxy', 'mastic', 'thinset', 'coatings', 'generalSmoothing', 'urethanes', 'highSpots', 'rainMarks', 'buildUp', 'resin', 'surfacePrep', 'nonSlip']
+    },
+    PCD: {
+      finish: 'textured',
+      tags: ['industrialCoverings']
+    }
+  }
+
   return (
-    <div>Application Search Page</div>
+    <>
+      <h1>Applicatoin Search</h1>
+        {tags.map(element => {
+          return <button>{element}</button>
+        })
+      }
+    </>
   )
 }
