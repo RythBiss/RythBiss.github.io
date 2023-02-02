f1e73bd1ca5ed1c5398734fd40205096ab064db8
RythBiss <dkschaarschmidt@gmail.com>
2023-02-02 15:24:56 -0500

    Image Banner
    Image banner is styled.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Background from '../images/concrete-1.jpg'
+import Hexs from '../images/Hexs.svg'
 
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <main>
+      <div className='image-banner'>
+        <img src={Background} alt='home-background'/>
+        <div className='image-overlay'>
+          <h3>American Made, Rental Tough</h3>
+          <h1>Your source for high quality equipment</h1>
+          <img src={Hexs} alt='hex art'></img>
+        </div>
+      </div>
+      <div id='products-div'>
+        <div id='appfinder-quicklink'>
+          <h2>Try our application solver tool</h2>
+          <button>but</button>
+        </div>
+      </div>
+    </main>
   )
 }
